0f6049bbb6265e0c2a28caaee06aee8f6ed728c1
Sand-raM <umurazawigasabo@gmail.com>
2024-08-16 22:01:53 +0300

    middleware for authentication

---
+++ SpendWise/middleware/AuthMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid token or user not found' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
