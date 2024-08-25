faa601680d9e1959184abfa25428ab332b41cc5e
Sand-raM <umurazawigasabo@gmail.com>
2024-08-25 04:54:26 +0300

    update

--- SpendWise/spendwise-backend/routes/LoginRoute.js
+++ SpendWise/spendwise-backend/routes/LoginRoute.js
@@ -11,12 +11,23 @@ router.post('/login', async (req, res) => {
     if (!user) {
       return res.status(401).send('Invalid email or password');
     }
+
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
       return res.status(401).send('Invalid email or password');
     }
+
     const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-    res.send({ token });
+
+    // Return user information along with the token
+    res.send({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('Error logging in');
