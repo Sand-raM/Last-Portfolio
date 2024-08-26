c5205119bb1462c725fdef35d42837d42b5a4d34
Sand-raM <umurazawigasabo@gmail.com>
2024-08-26 16:29:42 +0300

    the budget model

--- SpendWise/spendwise-backend/models/Budget.js
+++ SpendWise/spendwise-backend/models/Budget.js
@@ -7,7 +7,9 @@ const budgetSchema = new mongoose.Schema({
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  target: { type: Number, default: 0 },
+  spent: { type: Number, default: 0 }
 });
 
 const Budget = mongoose.model('Budget', budgetSchema);
